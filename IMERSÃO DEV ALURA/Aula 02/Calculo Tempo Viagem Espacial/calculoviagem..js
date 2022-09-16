61181d5f968110d8bc0e7640ecc0543b3bc66191
matheusfurts <113365070+matheusfurts@users.noreply.github.com>
2022-09-16 17:39:54 -0300

    Delete IMERSÃO DEV ALURA directory

--- IMERSÃO DEV ALURA/Aula 02/Calculo Tempo Viagem Espacial/calculoviagem..js
+++
@@ -1,9 +0,0 @@
-function calcular() {
-    var localA = window.document.getElementById("localA").value;
-    var localB = window.document.getElementById("localB").value;
-    var dist = window.document.getElementById("dist");
-    var res = window.document.getElementById("res");
-    var calcula = (Number(dist.value) / 9.5).toFixed(2);
-    res.innerHTML = `O tempo para ir de ${localA} para ${localB} é de ${calcula} anos luz.`;
-  }
-  
